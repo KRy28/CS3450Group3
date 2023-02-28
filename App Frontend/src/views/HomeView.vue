a2b304a57477266b8954d282fccd1b1962d2520a
JaydenLintz <lintzman84@gmail.com>
2023-02-27 23:27:30 -0700

    Created landing page

--- App Frontend/src/views/HomeView.vue
+++ App Frontend/src/views/HomeView.vue
@@ -2,8 +2,37 @@
 import TheWelcome from "../components/TheWelcome.vue";
 </script>
 
+<style>
+.buttons{
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 0.25rem;
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
+  cursor: pointer;
+  transition: background-color 0.2s;
+  display: block;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  text-decoration: none;
+  margin: 10px;
+}
+.buttons:hover {
+  background-color: #0069d9; /* darken background color on hover */
+}
+</style>
+
 <template>
   <main>
-    <TheWelcome />
+    <div style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;">
+      <center>
+            <h1>Dan's Car Barn</h1>
+            <h2>Rent-A-Car</h2>
+            <RouterLink class="buttons" to="/search">Search </RouterLink>
+            <RouterLink class="buttons" to="/about">About Us</RouterLink>
+            <RouterLink class="buttons" to="/contact">Contact Us</RouterLink>
+            <RouterLink class="buttons" to="/login">Log In</RouterLink>
+      </center>
+  </div>
   </main>
 </template>
