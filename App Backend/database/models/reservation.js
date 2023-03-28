4c53926d0bf318bf2a49e17254099760ebeb1d57
KRy28 <A02327076@usu.edu>
2023-03-28 13:46:50 -0600

    added an attempt at the reservation model

--- App Backend/database/models/reservation.js
+++ App Backend/database/models/reservation.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Car extends Model {
+  class Reservation extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -11,19 +11,15 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Car.hasMany(models.Reservation)
+      Reservation.belongsTo(models.User);
+      Reservation.belongsTo(models.Car);
     }
   }
-  Car.init({
-    make: DataTypes.STRING,
-    model: DataTypes.STRING,
-    year: DataTypes.INTEGER,
-    condition: DataTypes.STRING,
-    description: DataTypes.STRING,
-    rate: DataTypes.FLOAT
+  Reservation.init({
+    
   }, {
     sequelize,
-    modelName: 'Car',
+    modelName: 'Reservation',
   });
-  return Car;
+  return Reservation;
 };
