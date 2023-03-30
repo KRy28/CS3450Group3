cf0eb4729c14552191da4b41131ccde649ea8d13
Jayden Lintz <A02298168@usu.edu>
2023-03-30 16:15:47 -0600

    Car List and Details are sort of working. CORS issue, so need to use chrome extension to get past for now.

--- App Frontend/src/views/DetailView.vue
+++ App Frontend/src/views/DetailView.vue
@@ -1,13 +1,42 @@
-<script setup>
-import TheDetail from "../components/TheDetail.vue";
+<script>
+export default {
+  data() {
+  	return {
+      json: undefined,
+      car: undefined
+    }
+	},
 
+  methods: {
+    fetch_info() {
+            fetch("http://localhost:3000/cars/json")
+                .then(r => r.json())
+                .then(json => {
+                    this.json = json
+                    console.log(json)
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+        },
+  }
+  
+}
 </script>
 
 <template>
   <main>
     <center>
-      <h1>Car Details</h1>
-      <p>{{ this.$route.params.id }}</p>
+      {{ fetch_info() }}
+      <li v-for="car in json" style="list-style-type: none;">
+        <h1 v-if="car.id == this.$route.params.id">{{ car.make }} {{ car.model }}</h1>
+        <p v-if="car.id == this.$route.params.id">Year: {{ car.year }}</p>
+        <p v-if="car.id == this.$route.params.id">Condition: {{ car.condition }}</p>
+        <p v-if="car.id == this.$route.params.id">Cost: ${{ car.rate.toLocaleString('en', {useGrouping:true}) }}/Day</p>
+        <p v-if="car.id == this.$route.params.id">{{ car.description }}</p>
+        <RouterLink v-if="car.id == this.$route.params.id" class="checkoutButton" :to="`/calendar`">Rent</RouterLink>
+
+      </li>
     </center>
   </main>
 </template>
