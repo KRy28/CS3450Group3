ebc59b24b5bafcef3eaf61ae421e258fe793dd49
Ethan <ethanchristensen01@gmail.com>
2023-03-31 20:38:16 -0600

    Removed components made during project generation

--- App Frontend/src/main.js
+++ App Frontend/src/main.js
@@ -4,15 +4,6 @@ import App from "./App.vue";
 import router from "./router";
 import "./assets/main.css";
 
-// import VueSlider from 'vue-slider-component'
-// import 'vue-slider-component/theme/default.css'
-
-// Vue.component('VueSlider', VueSlider)
-
-//import VCalendar from 'v-calendar';
-
-// import 'v-calendar/lib/v-calendar.min.css';
-
 const app = createApp(App);
 
 app.use(createPinia());
