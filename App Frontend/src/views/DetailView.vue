91965427577066cf89b26de9a0fffb0bf6d8e380
Jaxson Madison <a02336244@aggies.usu.edu>
2023-04-11 11:38:20 -0600

    Styled the details page slightly

--- App Frontend/src/views/DetailView.vue
+++ App Frontend/src/views/DetailView.vue
@@ -1,17 +1,33 @@
 <template>
   <main>
     <center>
+      <!-- Call the fetch_info method to fetch car data -->
       {{ fetch_info() }}
+      
+      <!-- Loop through each car in the json data and display its info -->
       <li v-for="car in json" style="list-style-type: none;">
+        <!-- Display the car's make and model if its id matches the id in the URL -->
         <h1 v-if="car.id == this.$route.params.id">{{ car.make }} {{ car.model }}</h1>
+        
+        <!-- Display the car's image if its id matches the id in the URL -->
         <img v-if="car.id == this.$route.params.id" :src="car.image" style="height:400px">
-
+        
+        <!-- Display the car's year if its id matches the id in the URL -->
         <p v-if="car.id == this.$route.params.id">Year: {{ car.year }}</p>
+        
+        <!-- Display the car's condition if its id matches the id in the URL -->
         <p v-if="car.id == this.$route.params.id">Condition: {{ car.condition }}</p>
+        
+        <!-- Display the car's daily rate if its id matches the id in the URL -->
         <p v-if="car.id == this.$route.params.id">Cost: ${{ car.rate.toLocaleString('en', {useGrouping:true}) }}/Day</p>
-        <p v-if="car.id == this.$route.params.id">{{ car.description }}</p>
+        
+        <!-- Display the car's description if its id matches the id in the URL -->
+        <div v-if="car.id == this.$route.params.id" class="description">
+          <p>{{ car.description }}</p>
+        </div>
+        
+        <!-- Display a link to the calendar page if the car's id matches the id in the URL -->
         <RouterLink v-if="car.id == this.$route.params.id" class="checkoutButton" :to="`/calendar`">Rent</RouterLink>
-
       </li>
     </center>
   </main>
@@ -28,17 +44,29 @@ export default {
 
   methods: {
     fetch_info() {
-            fetch("http://localhost:3000/cars/json")
-                .then(r => r.json())
-                .then(json => {
-                    this.json = json
-                    console.log(json)
-                })
-                .catch(error => {
-                    console.log(error);
-                })
-        },
+      // Fetch the car data from the JSON API
+      fetch("http://localhost:3000/cars/json")
+        .then(r => r.json())
+        .then(json => {
+          // Set the json data to the component's data
+          this.json = json
+          console.log(json)
+        })
+        .catch(error => {
+          console.log(error);
+        })
+    },
   }
   
 }
 </script>
+
+<style>
+  /* Style the description box */
+  .description {
+    border: 1px solid black;
+    padding: 10px;
+    margin-top: 10px;
+    max-width: 800px;
+  }
+</style>
