711b7070ec04b41d5e97b6eea99831c809d69dce
Jaxson Madison <a02336244@aggies.usu.edu>
2023-04-16 17:35:31 -0600

    Got the confirmation screen working for checkout

--- App Frontend/src/components/RentalConfirmation.vue
+++ App Frontend/src/components/RentalConfirmation.vue
@@ -1,93 +1,76 @@
-<!-- src/components/RentalConfirmation.vue -->
-
 <template>
-    <div class="rental-confirmation">
+    <div class="container">
       <h1>Rental Confirmation</h1>
-      <div class="rental-info">
-        <div v-if="car">{{ car.make }} {{ car.model }}</div>
-        <img :src="car.image" style="height: 200px">
+      <div v-if="car" class="car-details">
+        <h2>{{ car.make }} {{ car.model }}</h2>
+        <img :src="car.image" alt="Car image" class="car-image" />
         <p>Year: {{ car.year }}</p>
         <p>Condition: {{ car.condition }}</p>
-        <p>Cost: ${{ car.rate }}/Day</p>
-        <p>Start Date: {{ startDate }}</p>
-        <p>End Date: {{ endDate }}</p>
-        <p>Total Days: {{ totalDays }}</p>
-        <p>Total Price: ${{ totalPrice }}</p>
+        <p>Description: {{ car.description }}</p>
+        <p>Rate: ${{ car.rate }}/Day</p>
+        <p>Reservation dates: {{ startDate }} - {{ endDate }}</p>
       </div>
     </div>
   </template>
   
+  <script>
+  import { ref, onMounted } from "vue";
+  import { useRoute } from "vue-router";
   
-<script>
-import { computed } from "vue";
-import { useRoute } from "vue-router";
-import { useStore } from "vuex";
-
-export default {
-  setup() {
-    const route = useRoute();
-    const store = useStore();
-
-    const car = computed(() => store.state.selectedCar);
-    const startDate = computed(() => route.params.startDate);
-    const endDate = computed(() => route.params.endDate);
-
-    const totalDays = computed(() => {
-      if (!startDate.value || !endDate.value) {
-        return 0;
-      }
-      const start = new Date(startDate.value);
-      const end = new Date(endDate.value);
-      return (end - start) / (1000 * 60 * 60 * 24) + 1;
-    });
-
-    const totalPrice = computed(() => {
-      if (!car.value || !totalDays.value) {
-        return 0;
-      }
-      return car.value.rate * totalDays.value;
-    });
-
-    return {
-      car,
-      startDate,
-      endDate,
-      totalDays,
-      totalPrice,
-    };
-  },
-};
-
-</script>
-
+  export default {
+    setup() {
+      const route = useRoute();
+      const carId = route.params.carId;
+      const startDate = route.params.startDate;
+      const endDate = route.params.endDate;
+      const car = ref(null);
   
-  <style scoped>
-  .rental-confirmation {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background-color: #f7f7f7;
-    border-radius: 5px;
-    padding: 20px;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+      const fetchCarDetails = async () => {
+        const response = await fetch(`http://localhost:3000/cars/json`);
+        const cars = await response.json();
+        car.value = cars.find(c => c.id == carId);
+      };
+  
+      onMounted(() => {
+        fetchCarDetails();
+      });
+  
+      return {
+        car,
+        startDate,
+        endDate,
+      };
+    },
+  };
+  </script>
+  
+  <style>
+  .container {
+    font-family: Segoe UI, Tahoma, Geneva, Verdana, sans-serif;
     max-width: 800px;
     margin: 0 auto;
+    padding: 20px;
   }
   
-  h1 {
-    font-size: 24px;
-    margin-bottom: 20px;
+  .car-details {
+    text-align: center;
+    border: 1px solid #ddd;
+    padding: 20px;
+    border-radius: 5px;
+    background-color: #f8f8f8;
+    margin-top: 20px;
   }
   
-  h2 {
-    font-size: 20px;
-    margin-bottom: 10px;
+  .car-image {
+    width: 100%;
+    max-width: 400px;
+    height: auto;
+    border-radius: 5px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   }
   
-  .rental-info {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  p {
+    margin: 10px 0;
   }
   </style>
   
--- App Frontend/src/views/DetailView.vue
+++ App Frontend/src/views/DetailView.vue
@@ -1,70 +1,71 @@
 <template>
-  <main v-if="car">
+  <main>
     <div style="font-family: Segoe UI, Tahoma, Geneva, Verdana, sans-serif">
       <center>
-        <h1>{{ car.make }} {{ car.model }}</h1>
-        <img :src="car.image" style="height:400px">
-        <p>Year: {{ car.year }}</p>
-        <p>Condition: {{ car.condition }}</p>
-        <p>Cost: ${{ car.rate.toLocaleString('en', {useGrouping:true}) }}/Day</p>
-        <div class="description">
-          <p>{{ car.description }}</p>
-        </div>
-        <RouterLink class="checkoutButton" :to="`/calendar`">Rent</RouterLink>
+        
+        <!-- Loop through each car in the json data and display its info -->
+        <li v-for="car in json" style="list-style-type: none;">
+          <!-- Display the car's make and model if its id matches the id in the URL -->
+          <h1 v-if="car.id == this.$route.params.id">{{ car.make }} {{ car.model }}</h1>
+          
+          <!-- Display the car's image if its id matches the id in the URL -->
+          <img v-if="car.id == this.$route.params.id" :src="car.image" style="height:400px">
+          
+          <!-- Display the car's year if its id matches the id in the URL -->
+          <p v-if="car.id == this.$route.params.id">Year: {{ car.year }}</p>
+          
+          <!-- Display the car's condition if its id matches the id in the URL -->
+          <p v-if="car.id == this.$route.params.id">Condition: {{ car.condition }}</p>
+          
+          <!-- Display the car's daily rate if its id matches the id in the URL -->
+          <p v-if="car.id == this.$route.params.id">Cost: ${{ car.rate.toLocaleString('en', {useGrouping:true}) }}/Day</p>
+          
+          <!-- Display the car's description if its id matches the id in the URL -->
+          <div v-if="car.id == this.$route.params.id" class="description">
+            <p>{{ car.description }}</p>
+          </div>
+          
+          <!-- Display a link to the calendar page if the car's id matches the id in the URL -->
+          <RouterLink v-if="car.id == this.$route.params.id" class="checkoutButton" :to="`/calendar`">Rent</RouterLink>
+        </li>
       </center>
+
     </div>
   </main>
-  <div v-else> <!-- Add a loading message while car data is being fetched -->
-    <p>Loading car details...</p>
-  </div>
 </template>
 
 <script>
-import { ref, computed, onMounted } from "vue";
-import { useRoute, useRouter } from "vue-router";
-import { useStore } from "vuex";
-
 export default {
-  setup() {
-    const route = useRoute();
-    const router = useRouter();
-    const store = useStore();
+  data() {
+  	return {
+      json: undefined,
+      car: undefined
+    }
+	},
 
-    const car = ref(null);
+  async created(){
+        await this.fetch_info();
+    },
 
-    const fetchCar = async () => {
-      const response = await fetch(
-        'http://localhost:3000/cars/json'
-        // `https://api.example.com/cars/${route.params.id}`
-      );
-      car.value = await response.json();
-    };
 
-    onMounted(() => {
-      fetchCar();
-    });
-
-    const startDate = ref("");
-    const endDate = ref("");
-
-    const submitReservation = () => {
-      store.dispatch("setSelectedCar", car.value);
-      router.push({
-        name: "RentalConfirmation",
-        params: { carId: car.value.id, startDate: startDate.value, endDate: endDate.value },
-      });
-    };
-
-    return {
-      car,
-      startDate,
-      endDate,
-      submitReservation,
-    };
-  },
-};
+  methods: {
+    fetch_info() {
+      // Fetch the car data from the JSON API
+      return fetch("http://localhost:3000/cars/json")
+        .then(r => r.json())
+        .then(json => {
+          // Set the json data to the component's data
+          this.json = json
+          console.log(json)
+        })
+        .catch(error => {
+          console.log(error);
+        })
+    },
+  }
+  
+}
 </script>
-
 
 <style>
   /* Style the description box */
