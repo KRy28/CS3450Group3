ff28cb270e9a1c427d6c326a55a69b1c40374314
Jaxson Madison <a02336244@aggies.usu.edu>
2023-04-16 20:33:15 -0600

    Fixed port

--- App Backend/app.js
+++ App Backend/app.js
@@ -16,7 +16,7 @@ const app = express();
 
 const cors = require('cors');
 const corsOptions = {
-  origin: 'http://localhost:5175', // Replace with your frontend's URL
+  origin: 'http://localhost:5173', // Replace with your frontend's URL
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }
