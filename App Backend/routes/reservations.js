15b0da55e8920f79f013def6698f84e0c20c2082
KRy28 <A02327076@usu.edu>
2023-04-18 12:54:39 -0600

    working reservations

--- App Backend/routes/reservations.js
+++ App Backend/routes/reservations.js
@@ -30,45 +30,47 @@ router.get('/add/:car/:start/:stop', async function(req, res, next) {
   const start_date = new Date(req.params.start);
   const stop_date = new Date(req.params.stop);
   const car_id = parseInt(req.params.car);
-  const car = Car.findOne({ where: { id:car_id }}) 
-  try {
-    const person = Person.findOne({ where: { username: req.user.username}})
-  const reservations = await Reservation.findAll({ where: { car_id: car_id }});
-  function dateRangesIntersect(startA, endA, startB, endB) {
-    return (startA <= endB && endA >= startB);
-  };
-  var check = true;
-  for (var i = 0, size = reservations.length; i < size ; i++) {
-   var item = reservations[i];
-   var start = new Date(item.start)
-   var stop = new Date(item.stop)
-   if (dateRangesIntersect(start_date, stop_date, start, stop)) {
-      check = false;
-   }; 
-  };
-  const diffTime = Math.abs(stop_date - start_date);
-  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
-  const cost = diffDays * car.rate; 
-  if (check && person.wallet >= cost) {
-    const reservation = await Reservation.create({
-      start: start_date,
-      stop: stop_date,
-      person_id: req.user.id,
-      car_id: car_id
-    });
-    person.wallet = person.wallet - cost;
-    person.save();
-    res.json("Success");
-  } else if (!check) {
-    res.json("OverlapErr");
-  } else if (person.wallet < cost) {
-    res.json("FundsErr");
-  } else {
-    res.json("unknownErr")
-  }; 
-  } catch(err) {
+  const car = await Car.findOne({ where: { id:car_id }}) 
+  //try {
+    const person = await Person.findOne({ where: { username: req.user.username}})
+    const reservations = await Reservation.findAll({ where: { car_id: car_id }});
+    function dateRangesIntersect(startA, endA, startB, endB) {
+      return (startA <= endB && endA >= startB);
+    };
+    var check = true;
+    for (var i = 0, size = reservations.length; i < size ; i++) {
+      var item = reservations[i];
+      var start = new Date(item.start)
+      var stop = new Date(item.stop)
+      if (dateRangesIntersect(start_date, stop_date, start, stop)) {
+        check = false;
+      }; 
+    };
+    const diffTime = Math.abs(stop_date - start_date);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+    const cost = diffDays * car.rate; 
+
+    console.log(car, car.rat) 
+    if (check && person.wallet >= cost) {
+      const reservation = await Reservation.create({
+        start: start_date,
+        stop: stop_date,
+        person_id: req.user.id,
+        car_id: car_id
+      });
+      person.wallet = person.wallet - cost;
+      person.save();
+      res.json("Success");
+    } else if (!check) {
+      res.json("OverlapErr");
+    } else if (person.wallet < cost) {
+      res.json("FundsErr");
+    } else {
+      res.json("unknownErr")
+    }; 
+  /*} catch(err) {
     res.json("LoggedOutErr")
-  };
+  };*/
 });
 
 module.exports = router;
