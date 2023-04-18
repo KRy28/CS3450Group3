7be3252d49ddcf468553b8b9b679dc6773267539
KRy28 <A02327076@usu.edu>
2023-04-18 13:08:42 -0600

    added reservations features

--- App Backend/routes/reservations.js
+++ App Backend/routes/reservations.js
@@ -21,56 +21,82 @@ router.param('car', 'start', 'stop', function(req, res, next, car, start, stop) 
    
 });*/
 
-router.get('/json', async function(req, res, next) {
+router.get('/get', async function(req, res, next) {
   const reservations = await Reservation.findAll()
   res.json(reservations);
 });
 
+
+router.get('/get/car/:car', async function(req, res, next) {
+  const car_id = parseInt(req.params.car);
+  const reservations = await Reservation.findAll({ where: { car_id: car_id}});
+  res.json(reservations);
+});
+
+router.get('/get/user', async function(req, res, next) {
+  if (req.user == undefined) {
+    res.json("LoggedOutErr")
+  } else {
+    const person = await Person.findOne({ where: { username: req.user.username}})
+    const reservations = await Reservation.findAll()
+    res.json(reservations);
+  };
+});
+
+router.get('/json', async function(req, res, next) {
+  const reservations = await Reservation.findAll()
+  res.json(reservations);
+});
 router.get('/add/:car/:start/:stop', async function(req, res, next) {
   const start_date = new Date(req.params.start);
   const stop_date = new Date(req.params.stop);
   const car_id = parseInt(req.params.car);
   const car = await Car.findOne({ where: { id:car_id }}) 
-  //try {
-    const person = await Person.findOne({ where: { username: req.user.username}})
-    const reservations = await Reservation.findAll({ where: { car_id: car_id }});
-    function dateRangesIntersect(startA, endA, startB, endB) {
-      return (startA <= endB && endA >= startB);
-    };
-    var check = true;
-    for (var i = 0, size = reservations.length; i < size ; i++) {
-      var item = reservations[i];
-      var start = new Date(item.start)
-      var stop = new Date(item.stop)
-      if (dateRangesIntersect(start_date, stop_date, start, stop)) {
-        check = false;
+  if (car == undefined) {
+    res.json("CarIdErr");
+  } else if (person == undefined) {
+    res.json("LoggedOutErr");
+  } else {
+    try {
+      const person = await Person.findOne({ where: { username: req.user.username}})
+      const reservations = await Reservation.findAll({ where: { car_id: car_id }});
+      function dateRangesIntersect(startA, endA, startB, endB) {
+        return (startA <= endB && endA >= startB);
+      };
+      var check = true;
+      for (var i = 0, size = reservations.length; i < size ; i++) {
+        var item = reservations[i];
+        var start = new Date(item.start)
+        var stop = new Date(item.stop)
+        if (dateRangesIntersect(start_date, stop_date, start, stop)) {
+          check = false;
+        }; 
+      };
+      const diffTime = Math.abs(stop_date - start_date);
+      const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+      const cost = diffDays * car.rate; 
+	    
+      if (check && person.wallet >= cost) {
+        const reservation = await Reservation.create({
+          start: start_date,
+          stop: stop_date,
+          person_id: req.user.id,
+          car_id: car_id
+        });
+        person.wallet = person.wallet - cost;
+        person.save();
+        res.json("Success");
+      } else if (!check) {
+        res.json("OverlapErr");
+      } else if (person.wallet < cost) {
+        res.json("FundsErr");
+      } else {
+        res.json("unknownErr")
       }; 
+    } catch(err) {
+      res.json("unknownFatalErr")
     };
-    const diffTime = Math.abs(stop_date - start_date);
-    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
-    const cost = diffDays * car.rate; 
-
-    console.log(car, car.rat) 
-    if (check && person.wallet >= cost) {
-      const reservation = await Reservation.create({
-        start: start_date,
-        stop: stop_date,
-        person_id: req.user.id,
-        car_id: car_id
-      });
-      person.wallet = person.wallet - cost;
-      person.save();
-      res.json("Success");
-    } else if (!check) {
-      res.json("OverlapErr");
-    } else if (person.wallet < cost) {
-      res.json("FundsErr");
-    } else {
-      res.json("unknownErr")
-    }; 
-  /*} catch(err) {
-    res.json("LoggedOutErr")
-  };*/
+  };
 });
 
 module.exports = router;
